2bea589c9308ed8325cf525241ce439322a7f4ce
Isabel Marquinez <75847691+Chavelax@users.noreply.github.com>
2021-05-16 18:51:55 -0500

    Update MainActivity.java

--- app/src/main/java/com/utc/appparking/MainActivity.java
+++ app/src/main/java/com/utc/appparking/MainActivity.java
@@ -1,8 +1,16 @@
 package com.utc.appparking;
-
+/*
+@utor: EnmaMarqui
+@utor: KevinSantana
+creado: 16/05/2021
+modificado: 18/05/2021
+descripcion: ventana  de inicio de sesion acceso, crear cuenta para parueadero
+ */
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirPantallaRegistro(View vista){
+        Intent pantallaRegistrar = new Intent(getApplicationContext(),activity_registro.class);//item para invocar el registro
+        startActivity(pantallaRegistrar);//abrir pantalla registrar
+
     }
 }
