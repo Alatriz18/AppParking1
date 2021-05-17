8d4dfffa67d1b5b608b264cdcf4159b8424974b7
Isabel Marquinez <75847691+Chavelax@users.noreply.github.com>
2021-05-17 16:37:39 -0500

    Update BaseDatos.java

--- app/src/main/java/com/utc/appparking/BaseDatos.java
+++ app/src/main/java/com/utc/appparking/BaseDatos.java
@@ -7,10 +7,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class BaseDatos extends SQLiteOpenHelper {
     public static final String nombreBdd="bdd_parking";//definiendo el nombre de la bdd
     public static final int versionBdd=1;
-    public static final String tablaUsuario="create table usuario(id_usu integer primary key autoinrement," +
-            "apellidos_usu text,nombres_usu text, email_usu text, password_usu text, telefono_usu text, direccion_usu text)" ;//definiendo la estructura de la tabla usuario
-//constructor
-    public BaseDatos(Context contexto) {super(contexto,nombreBdd, factory null, versionBdd)}
+    public static final String tablaUsuario="create table usuario(id_usu integer primary key autoincrement," +
+            "apellidos_usu text,nombres_usu text, email_usu text, password_usu text, telefono_usu text, direccion_usu text)";//Estructura de tabla usuario
+    //constructor
+
+    public BaseDatos (Context contexto){super(contexto, nombreBdd,null, versionBdd);}
+
     //proceso1: metodo q ejecuta automaticamente al construir la clase Base Datos
     @Override
     public void onCreate(SQLiteDatabase db) {
