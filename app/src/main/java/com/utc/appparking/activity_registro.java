6bf3daa6213315b232f0164177fd5836e8b42613
Alatriz18 <kevin.santana4621@utc.edu.ec>
2021-05-16 22:05:00 -0500

    Update activity_registro.java

--- app/src/main/java/com/utc/appparking/activity_registro.java
+++ app/src/main/java/com/utc/appparking/activity_registro.java
@@ -3,12 +3,25 @@ package com.utc.appparking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+/*
+@utor: EnmaMarqui
+@utor: KevinSantana
+creado: 16/05/2021
+modificado: 18/05/2021
+descripcion:Pantalla para registrar usuarios y almacenar en sqlite
+ */
 public class activity_registro extends AppCompatActivity {
-
+    //proceso 1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+
+    //proceso cerrar
+    public void cerrarPantallaRegistro(View vista){
+       finish();//cierra pantalla registro
     }
 }
