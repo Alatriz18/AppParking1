ab4bd88bafb7d8c4ec5c18192d1d8eca9f8eb597
Isabel Marquinez <75847691+Chavelax@users.noreply.github.com>
2021-05-17 18:15:34 -0500

    Update activity_registro.java

--- app/src/main/java/com/utc/appparking/activity_registro.java
+++ app/src/main/java/com/utc/appparking/activity_registro.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /*
 @utor: EnmaMarqui
@@ -13,15 +15,50 @@ modificado: 18/05/2021
 descripcion:Pantalla para registrar usuarios y almacenar en sqlite
  */
 public class activity_registro extends AppCompatActivity {
+    EditText txtApellidos, txtNombres, txtCorreo, txtIngreseClave, txtConfirmeClave, txtNumero, txtDireccion;
+    BaseDatos miBase;
+
     //proceso 1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        txtApellidos=(EditText)findViewById(R.id.txtApellidos);
+        txtNombres=(EditText)findViewById(R.id.txtNombres);
+        txtCorreo=(EditText)findViewById(R.id.txtCorreo);
+        txtIngreseClave=(EditText)findViewById(R.id.txtIngreseClave);
+        txtConfirmeClave=(EditText)findViewById(R.id.txtConfirmeClave);
+        txtNumero=(EditText)findViewById(R.id.txtNumero);
+        txtDireccion=(EditText)findViewById(R.id.txtDireccion);
+
+        miBase=new BaseDatos(getApplicationContext());
     }
 
-    //proceso cerrar
+    //proceso(2) cerrar
     public void cerrarPantallaRegistro(View vista){
        finish();//cierra pantalla registro
     }
+
+    //poroceso(3)
+    public void registrarUsuario(View vista){
+        String apellidos=txtApellidos.getText().toString();
+        String nombres=txtNombres.getText().toString();
+        String email=txtCorreo.getText().toString();
+        String password=txtIngreseClave.getText().toString();
+        String passwordx=txtConfirmeClave.getText().toString();
+        String telefono=txtNumero.getText().toString();
+        String direccion=txtDireccion.getText().toString();
+
+        if (password.equals(passwordx)){
+            miBase.registrarUsuario(apellidos, nombres, email, password, telefono, direccion);
+            //Toast.makeText(getApplicationContext(), "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getApplicationContext(), )
+            Toast.makeText(getApplicationContext(), "Datos Registrados Con Exito", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(getApplicationContext(), "Las contaseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
 }
