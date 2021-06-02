3ddf45154a4b6f1acec166d7163f96dc2828a7b9
Isabel Marquinez <75847691+Chavelax@users.noreply.github.com>
2021-06-01 19:36:38 -0500

    Update MainActivity.java

--- app/src/main/java/com/utc/appparking/MainActivity.java
+++ app/src/main/java/com/utc/appparking/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 /*
 @utor: EnmaMarqui
 @utor: KevinSantana
@@ -15,15 +16,27 @@ descripcion: ventana  de inicio de sesion acceso, crear cuenta para parueadero
 
 public class MainActivity extends AppCompatActivity {
 
+    //Entrada
+    EditText loginEmail, loginClave;     //creo objetos
+    BaseDatos bdd;//creo objetito tipo bdd
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //proceso 1 mapeo de elementos Xml a objetos JAVA
+        loginEmail = (EditText) findViewById(R.id.loginEmail);
+        loginClave = (EditText) findViewById(R.id.loginClave);
+        bdd=new BaseDatos(getApplicationContext());
     }
 
+    //proceso2
     public void abrirPantallaRegistro(View vista){
         Intent pantallaRegistrar = new Intent(getApplicationContext(),activity_registro.class);//item para invocar el registro
         startActivity(pantallaRegistrar);//abrir pantalla registrar
 
     }
+    //proceso 3
+
 }
